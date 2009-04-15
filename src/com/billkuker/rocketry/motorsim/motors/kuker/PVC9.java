621545b893afc956131dd845f1845a5e1f1f8253
Bill Kuker <bkuker@billkuker.com>
2009-04-15 18:44:56 +0000

    Added some real motors

---
+++ src/com/billkuker/rocketry/motorsim/motors/kuker/PVC9.java
@@ -0,0 +1,45 @@
+package com.billkuker.rocketry.motorsim.motors.kuker;
+
+import javax.measure.unit.NonSI;
+import javax.measure.unit.SI;
+
+import org.jscience.physics.amount.Amount;
+
+import com.billkuker.rocketry.motorsim.Burn;
+import com.billkuker.rocketry.motorsim.ConvergentDivergentNozzle;
+import com.billkuker.rocketry.motorsim.CylindricalChamber;
+import com.billkuker.rocketry.motorsim.Motor;
+import com.billkuker.rocketry.motorsim.fuel.KNSU;
+import com.billkuker.rocketry.motorsim.grain.CoredCylindricalGrain;
+import com.billkuker.rocketry.motorsim.grain.MultiGrain;
+import com.billkuker.rocketry.motorsim.visual.BurnPanel;
+
+public class PVC9 extends Motor {
+	public PVC9() throws Exception{
+		setFuel(new KNSU());
+		
+		CylindricalChamber c = new CylindricalChamber();
+		c.setLength(Amount.valueOf(200, SI.MILLIMETER));
+		c.setID(Amount.valueOf(30, SI.MILLIMETER));
+		setChamber(c);
+		
+		CoredCylindricalGrain g = new CoredCylindricalGrain();
+		g.setLength(Amount.valueOf(70, SI.MILLIMETER));
+		g.setOD(Amount.valueOf(29, SI.MILLIMETER));
+		g.setID(Amount.valueOf(8, SI.MILLIMETER));
+	
+		setGrain( new MultiGrain(g, 2) );
+		
+		ConvergentDivergentNozzle n = new ConvergentDivergentNozzle();
+		n.setThroatDiameter(Amount.valueOf(7.9, SI.MILLIMETER));
+		n.setExitDiameter(Amount.valueOf(9, SI.MILLIMETER));
+		n.setEfficiency(.87);
+		setNozzle(n);
+	}
+	
+	public static void main(String args[]) throws Exception{
+		PVC9 m = new PVC9();
+		Burn b = new Burn(m);
+		new BurnPanel(b).show();
+	}
+}
---
+++ src/com/billkuker/rocketry/motorsim/motors/nakka/KappaSB.java
@@ -0,0 +1,46 @@
+package com.billkuker.rocketry.motorsim.motors.nakka;
+
+import javax.measure.unit.NonSI;
+import javax.measure.unit.SI;
+
+import org.jscience.physics.amount.Amount;
+
+import com.billkuker.rocketry.motorsim.Burn;
+import com.billkuker.rocketry.motorsim.ConvergentDivergentNozzle;
+import com.billkuker.rocketry.motorsim.CylindricalChamber;
+import com.billkuker.rocketry.motorsim.Motor;
+import com.billkuker.rocketry.motorsim.fuel.KNSB;
+import com.billkuker.rocketry.motorsim.grain.CoredCylindricalGrain;
+import com.billkuker.rocketry.motorsim.grain.MultiGrain;
+import com.billkuker.rocketry.motorsim.visual.BurnPanel;
+
+public class KappaSB extends Motor{
+	public KappaSB() throws Exception{
+		setFuel(new KNSB());
+		
+		CylindricalChamber c = new CylindricalChamber();
+		c.setLength(Amount.valueOf(16, NonSI.INCH));
+		c.setID(Amount.valueOf(2.37, NonSI.INCH));
+		setChamber(c);
+		
+		CoredCylindricalGrain g = new CoredCylindricalGrain();
+		g.setLength(Amount.valueOf(3.8, NonSI.INCH));
+		g.setOD(Amount.valueOf(2.23, NonSI.INCH));
+		g.setID(Amount.valueOf(.75, NonSI.INCH));
+	
+		setGrain( new MultiGrain(g, 4) );
+		
+		ConvergentDivergentNozzle n = new ConvergentDivergentNozzle();
+		n.setThroatDiameter(Amount.valueOf(.502, NonSI.INCH));
+		n.setExitDiameter(Amount.valueOf(1.67, NonSI.INCH));
+		n.setEfficiency(.87);
+		setNozzle(n);
+	}
+	
+	public static void main(String args[]) throws Exception{
+		KappaSB m = new KappaSB();
+		Burn b = new Burn(m);
+		new BurnPanel(b).show();
+	}
+
+}
