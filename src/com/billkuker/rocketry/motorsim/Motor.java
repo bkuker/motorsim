493047fc1f0e3717854584447d258c0c9c23316e
Bill Kuker <bkuker@billkuker.com>
2009-07-08 23:13:09 +0000

    Beanified

--- src/com/billkuker/rocketry/motorsim/Motor.java
+++ src/com/billkuker/rocketry/motorsim/Motor.java
@@ -1,6 +1,6 @@
 package com.billkuker.rocketry.motorsim;
 
-public class Motor {
+public class Motor extends MotorPart{
 	private Chamber chamber;
 	private Grain grain;
 	private Nozzle nozzle;
@@ -13,7 +13,9 @@ public class Motor {
 	}
 	
 	public void setChamber(Chamber chamber) {
+		Chamber old = this.chamber;
 		this.chamber = chamber;
+		firePropertyChange("Chamber", old, chamber);
 	}
 	
 	public Grain getGrain() {
@@ -21,7 +23,9 @@ public class Motor {
 	}
 	
 	public void setGrain(Grain grain) {
+		Grain old = this.grain;
 		this.grain = grain;
+		firePropertyChange("Grain", old, grain);
 	}
 	
 	public Nozzle getNozzle() {
@@ -29,7 +33,9 @@ public class Motor {
 	}
 	
 	public void setNozzle(Nozzle nozzle) {
+		Nozzle old = nozzle;
 		this.nozzle = nozzle;
+		firePropertyChange("Nozzle", old, nozzle);
 	}
 
 	public Fuel getFuel() {
@@ -37,7 +43,9 @@ public class Motor {
 	}
 
 	public void setFuel(Fuel fuel) {
+		Fuel old = fuel;
 		this.fuel = fuel;
+		firePropertyChange("Fuel", old, fuel);
 	}
 
 	public String getName() {
@@ -45,6 +53,8 @@ public class Motor {
 	}
 
 	public void setName(String name) {
+		String old = name;
 		this.name = name;
+		firePropertyChange("Name", old, name);
 	}
 }
