d45a76a0861ce15f047cf69c87110e4c0ec8562b
Bill Kuker <bkuker@billkuker.com>
2010-11-23 00:06:00 +0000

    Resolve Fuel file URLS locally

--- src/com/billkuker/rocketry/motorsim/fuel/FuelResolver.java
+++ src/com/billkuker/rocketry/motorsim/fuel/FuelResolver.java
@@ -1,5 +1,9 @@
 package com.billkuker.rocketry.motorsim.fuel;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collections;
@@ -9,6 +13,7 @@ import java.util.Map;
 import java.util.Set;
 
 import com.billkuker.rocketry.motorsim.Fuel;
+import com.billkuker.rocketry.motorsim.io.MotorIO;
 
 public class FuelResolver {
 	public static class FuelNotFound extends Exception {
@@ -57,7 +62,16 @@ public class FuelResolver {
 	}
 
 	private static Fuel tryResolve(URI u) throws FuelNotFound {
-		throw new FuelNotFound();
+		File f = new File(u);
+		try {
+			Fuel fuel = MotorIO.readFuel(new FileInputStream(f));
+			add(fuel, u);
+			return fuel;
+		} catch (FileNotFoundException e) {
+			throw new FuelNotFound();
+		} catch (IOException e) {
+			throw new FuelNotFound();
+		}
 	}
 
 	public static void add(Fuel f, URI uri) {
